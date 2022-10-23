a07960c81131586598734cae2bfddcf765f6da9f
janarild.eileng <janarild.eileng@fhi.no>
2022-10-23 11:59:52 +0200

    Add project files.

---
+++ Kompetanse.Quiz.Stars/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+var tasks = Enumerable.Range(0, 2)
+    .Select( e=> Task.Run(() =>
+        {
+            Console.Write("*");
+             return;    
+        }));
+
+await Task.WhenAll(tasks);
+
+Console.Write(tasks.Count());
+
